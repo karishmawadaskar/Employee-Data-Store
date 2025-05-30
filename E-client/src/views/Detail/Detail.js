cccadea612b95c08a7c11435971be2ee9d2bd12b
karishmawadaskar <karishmawadaskar@gmail.com>
2025-05-31 00:13:40 +0530

    added button to detail page

--- E-client/src/views/Detail/Detail.js
+++ E-client/src/views/Detail/Detail.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './Detail.css';
-import { useParams } from 'react-router-dom';
+import {useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function Detail() {
+  const navigate = useNavigate();
   const [student, setStudent] = useState(null);
   const { id } = useParams();
 
@@ -31,24 +32,27 @@ function Detail() {
   //  Only runs once student is not null
   return (
     <div className="employee-detail-wrapper">
-  <h1 className="employee-heading">Employee Details</h1>
-  <div className="employee-card">
-    <div className="employee-image-section">
-      <img src={student?.image || "https://via.placeholder.com/150"} alt="Employee" />
+      <h1 className="employee-heading">Employee Details</h1>
+      <div className="employee-card">
+        <div className="employee-image-section">
+          <img src={student?.image || "https://via.placeholder.com/150"} alt="Employee" />
+        </div>
+        <div className="employee-info-section">
+          <h2>{student?.name}</h2>
+          <p><strong>ID:</strong> {student?.id}</p>
+          <p><strong>City:</strong> {student?.city}</p>
+          <p><strong>Age:</strong> {student?.age}</p>
+          <p><strong>Blood Group:</strong> {student?.bloodGroup}</p>
+          <p><strong>Role:</strong> {student?.post}</p>
+          <p><strong>Gender:</strong> {student?.gender}</p>
+          <p><strong>Birth Date:</strong> {student?.birthDate}</p>
+          <p><strong>Email:</strong> {student?.email}</p>
+        </div>
+        <button type="button" className="btn2 btn-secondary" onClick={() => navigate('/')}>
+          Home
+        </button>
+      </div>
     </div>
-    <div className="employee-info-section">
-      <h2>{student?.name}</h2>
-      <p><strong>ID:</strong> {student?.id}</p>
-      <p><strong>City:</strong> {student?.city}</p>
-      <p><strong>Age:</strong> {student?.age}</p>
-      <p><strong>Blood Group:</strong> {student?.bloodGroup}</p>
-      <p><strong>Role:</strong> {student?.post}</p>
-      <p><strong>Gender:</strong> {student?.gender}</p>
-      <p><strong>Birth Date:</strong> {student?.birthDate}</p>
-      <p><strong>Email:</strong> {student?.email}</p>
-    </div>
-  </div>
-</div>
 
   );
 }
